0cfc90adabf3e975504dcbf982e357eeec02872e
vitor hugo batista <vitor_hugobatista@hotmail.com>
2019-06-04 15:23:57 +0000

    Vitor - Home.hs

--- Funcionario.hs
+++
@@ -1,5 +0,0 @@
--- SELECT * FROM Funcionario
-getListFuncionarioR :: Handler Html
-getListFuncionarioR = do 
-    funcionarios <- runDB $ selectList [] [Asc FuncionarioNome]
-    defaultLayout $(whamletFile "templates/funcionario.hamlet")

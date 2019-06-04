be337a00021fb0466951d96a0d3330077bb3f156
vitor hugo batista <vitor_hugobatista@hotmail.com>
2019-06-04 15:21:27 +0000

    Vitor - listar funcionarios

--- Funcionario.hs
+++ Funcionario.hs
@@ -1,28 +1,5 @@
-getFuncionarioR :: TreinamentoId -> Handler Html
-getFuncionarioR treid = do
-    runDB $ get404 treid
-    msg <- getMessage
-    (widget,enctype) <- generateFormPost (formFuncionario treid)
-    defaultLayout $ do
-        addStylesheet $ StaticR css_bootstrap_css
-        [whamlet|
-            $maybe mensagem <- msg
-                ^{mensagem}
-            <form action=@{FuncionarioR treid} method=post enctype=#{enctype}>
-                ^{widget}
-                <input type="submit" value="Cadastrar">
-        |]
-
-postFuncionarioR :: TreinamentoId -> Handler Html
-postFuncionarioR treid = do 
-    ((res,_),_) <- runFormPost (formFuncionario treid)
-    case res of 
-        FormSuccess funcionario -> do 
-            _ <- runDB $ insert funcionario
-            setMessage [shamlet|
-                <h2>
-                    Funcionario CADASTRADO COM SUCESSO!
-            |]
-            redirect TreinamentoR
-        _ -> redirect HomeR
-    
+-- SELECT * FROM Funcionario
+getListFuncionarioR :: Handler Html
+getListFuncionarioR = do 
+    funcionarios <- runDB $ selectList [] [Asc FuncionarioNome]
+    defaultLayout $(whamletFile "templates/funcionario.hamlet")

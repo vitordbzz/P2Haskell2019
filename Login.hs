4e69e28c362ea699df1d28ee6e82603249624529
vitor hugo batista <vitor_hugobatista@hotmail.com>
2019-06-04 15:36:07 +0000

    Filipe - logout do login e get do admin

--- Login.hs
+++ Login.hs
@@ -1,45 +1,12 @@
-formLogin :: Form Usuario
-formLogin = renderBootstrap $ Usuario 
-        <$> areq emailField "E-mail:" Nothing
-        <*> areq passwordField "Senha: " Nothing
+postLogoutR :: Handler Html
+postLogoutR = do 
+    deleteSession "_ID"
+    redirect HomeR
     
-getLoginR :: Handler Html
-getLoginR = do 
-    (widget,enctype) <- generateFormPost formLogin
-    msg <- getMessage
-    defaultLayout $ do
+getAdminR :: Handler Html
+getAdminR = do 
+    defaultLayout $ do 
         [whamlet|
-            $maybe mensagem <- msg
-                ^{mensagem}
-            <form action=@{LoginR} method=post>
-                ^{widget}
-                <input type="submit" value="entrar">
+            <h1>
+                BEM-VINDO ADMIN
         |]
-
-postLoginR :: Handler Html
-postLoginR = do
-    ((res,_),_) <- runFormPost formLogin
-    case res of
-        FormSuccess (Usuario "root@root123.com" "root") -> do 
-            setSession "_ID" "root"
-            redirect AdminR
-        FormSuccess usuario -> do
-            usuBanco <- runDB $ getBy $ UniqueRestEmail (usuarioEmail usuario)
-            case usuBanco of 
-                Just usuarioValido -> do 
-                    if ((usuarioSenha usuario) == (usuarioSenha $ entityVal usuarioValido)) then do 
-                        setSession "_ID" (usuarioEmail $ entityVal usuarioValido)
-                        redirect HomeR
-                    else do
-                        setMessage [shamlet|
-                            <h1>
-                                Senha invalida
-                        |]
-                        redirect LoginR
-                        
-                Nothing -> do
-                    setMessage [shamlet|
-                        Usuario n encontrado
-                    |]
-                    redirect LoginR
-        _ -> redirect HomeR
